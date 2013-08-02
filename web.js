62b8edf4673491dcd7293cebc74977858847c73b
Ubuntu <ubuntu@ip-172-31-28-77.us-west-2.compute.internal>
2013-08-02 08:24:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+  response.send('Hello World, I AM THE BOSS!!! Ce ata i gceannas? TA MISE I GCEANNAS!!!')
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
